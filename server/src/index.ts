9c58bcfbbb88fef643d48411628bf37368bee3b6
Jun0613-spec <kumo5110@gmail.com>
2025-04-17 11:05:09 +0100

    feat. fixed bug

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL!,
+    origin: [process.env.CLIENT_URL!, process.env.PRODUCT_URL!],
     credentials: true
   })
 );
