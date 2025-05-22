ca05f0fc4bccb2882dce0f647f9e7967a946a78c
Jun0613-spec <kumo5110@gmail.com>
2025-05-22 09:45:23 +0100

    feat. keep the server with render slow start

--- server/src/index.ts
+++ server/src/index.ts
@@ -57,43 +57,43 @@ app.use("/api/bookings", bookingsRoute);
 
 const url = process.env.SERVER_URL!;
 
-const interval = 14 * 60 * 1000; // 14 minutes
+// const interval = 14 * 60 * 1000; // 14 minutes
 
-const reloadWebsite = () => {
-  const now = new Date();
+// const reloadWebsite = () => {
+//   const now = new Date();
 
-  const ukHour = new Intl.DateTimeFormat("en-GB", {
-    timeZone: "Europe/London",
-    hour: "numeric",
-    hour12: false
-  }).format(now);
+//   const ukHour = new Intl.DateTimeFormat("en-GB", {
+//     timeZone: "Europe/London",
+//     hour: "numeric",
+//     hour12: false
+//   }).format(now);
 
-  const hour = parseInt(ukHour, 10);
+//   const hour = parseInt(ukHour, 10);
 
-  if (hour >= 9 && hour < 21) {
-    axios
-      .get(url)
-      .then(() => {
-        console.log(
-          "✅ Pinged at",
-          now.toLocaleTimeString("en-GB", { timeZone: "Europe/London" })
-        );
-      })
-      .catch((error) => {
-        console.error("Ping error:", error.message);
-      });
-  } else {
-    console.log(
-      "Skipped ping at",
-      now.toLocaleTimeString("en-GB", { timeZone: "Europe/London" })
-    );
-  }
-};
+//   if (hour >= 9 && hour < 21) {
+//     axios
+//       .get(url)
+//       .then(() => {
+//         console.log(
+//           "✅ Pinged at",
+//           now.toLocaleTimeString("en-GB", { timeZone: "Europe/London" })
+//         );
+//       })
+//       .catch((error) => {
+//         console.error("Ping error:", error.message);
+//       });
+//   } else {
+//     console.log(
+//       "Skipped ping at",
+//       now.toLocaleTimeString("en-GB", { timeZone: "Europe/London" })
+//     );
+//   }
+// };
 
-if (process.env.NODE_ENV === "production") {
-  reloadWebsite();
-  setInterval(reloadWebsite, interval);
-}
+// if (process.env.NODE_ENV === "production") {
+//   reloadWebsite();
+//   setInterval(reloadWebsite, interval);
+// }
 
 const port = process.env.PORT || 3001;
 
